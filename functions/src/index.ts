fae7899948fcd29909c0bb7dc7a33ca431a3b815
Aldo Rey Balagulan <aldo.balagulan@gmail.com>
2018-12-28 10:54:23 +0800

    Add basic functions API

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as cors from 'cors';
+import { sample } from 'lodash';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +9,30 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const app = express();
+
+app.use(cors({ origin: true }));
+
+const news = [
+  {
+    title: "This cat is so sad",
+    blurb: "Alexa play Despacito",
+    image: "https://i.kym-cdn.com/entries/icons/original/000/026/489/crying.jpg"
+  },
+  {
+    title: "Butter makes cats sad",
+    blurb: "Please say no to lipids",
+    image: "https://i.kym-cdn.com/photos/images/newsfeed/001/384/535/295.jpg"
+  }
+];
+
+app.get('/news', (req, res) => {
+  res.send(sample(news));
+});
+
+app.post('/gottem', (req, res) => {
+  // Push notif logic here
+});
+
+exports.endpoints = functions.https.onRequest(app);
